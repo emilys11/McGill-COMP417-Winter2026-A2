92bd690d7a256fb1f59c26cbb2133c8ede85b496
David Meger <david.meger@gmail.com>
2026-01-30 10:47:29 -0500

    Adding starter code.
    This contains all files needed to get started coding A2. We will need to add some instructions in the README and then you will be able to follow the assignment right away.

---
+++ arm_rrt.py
@@ -0,0 +1,154 @@
+import numpy as np
+from tqdm import tqdm
+from scipy.spatial import cKDTree
+import random
+import argparse
+import robots
+
+class Node:
+    def __init__(self, point, parent=None):
+        self.point = np.array(point)
+        self.parent = parent
+
+class Tree:
+    def __init__(self, node_list, kdtree):
+        self.node_list = node_list
+        self.kdtree = kdtree
+    
+    def add(self,new_point):
+        self.node_list.append(new_point)
+        
+        if len(self.node_list) % 50 == 0:
+            data = [n.point for n in self.node_list]
+            self.kdtree = cKDTree(data)
+
+class RRT:
+    def __init__(self, step_size=0.1,max_iter=20,env_name="Free"):
+        self.controller = robots.Gen3LiteArmController(env_name=env_name)
+
+        self.start = Node(self.controller.getCurrentJointAngles())
+        self.goal = Node(self.controller.goal_angles)
+        self.rand_ranges = self.controller.getRanges()
+        self.step_size = step_size
+        self.max_iter = max_iter
+        self.start_tree = Tree([self.start],cKDTree([self.start.point]))
+        self.goal_tree = Tree([self.goal],cKDTree([self.goal.point]))
+        self.path_to_goal = []
+
+    # This a "stub" planning function. It adds random nodes always
+    # to the root of a start and goal tree. It shows many of the 
+    # syntax elements and helper functions you could use, but 
+    # you will have to fix and extend this to make it an RRT or 
+    # RRT-Connect planner that can solve the harder environments.
+    def plan(self):
+
+        # Add max_iter nodes to each start and goal tree
+        for k in tqdm(range(self.max_iter)):
+            rnd_point = self.sample()
+            if self.collision_free(rnd_point, self.start.point):
+                new_node = Node(rnd_point,self.start)
+                self.start_tree.add(new_node)
+
+            rnd_point = self.sample()
+            if self.collision_free(rnd_point, self.goal.point):
+                new_node = Node(rnd_point,self.goal)
+                self.goal_tree.add(new_node)
+
+        # Now add the same rnd_pnt to both trees, making a connection
+        # and completing the path start->goal
+        rnd_point = self.sample()   
+        if self.collision_free(rnd_point, self.start.point):
+            new_start_node = Node(rnd_point,self.start)
+            self.start_tree.add(new_start_node)
+
+        if self.collision_free(rnd_point, self.goal.point):
+            new_goal_node = Node(rnd_point,self.goal)
+            self.goal_tree.add(new_goal_node)
+
+        self.path_to_goal = self.extract_path(new_start_node,new_goal_node)
+        return True
+
+    def sample(self):
+        point = []
+        for i in range(0,len(self.rand_ranges[0])):
+            point.append(random.uniform(self.rand_ranges[0][i], self.rand_ranges[1][i]))
+        return np.array(point)
+
+    def nearest_node(self, point, tree):
+         _, idx = tree.kdtree.query(point)
+         return tree.node_list[idx]
+
+    def steer(self, from_node, to_point):
+        direction = to_point - from_node.point
+        distance = np.linalg.norm(direction)
+
+        if distance < self.step_size:
+            new_point = to_point
+        else:
+            direction = direction / distance
+            new_point = from_node.point + self.step_size * direction
+
+        new_node = Node(new_point)
+        new_node.parent = from_node
+        return new_node
+
+    def collision_free(self, p1, p2):
+        return self.controller.collision_free(p1,p2)
+
+    def reached_goal(self, node,goal=None):
+        if goal is None:
+            goal = self.goal
+        
+        return np.linalg.norm(node.point - goal.point) < self.step_size and self.collision_free(node.point,goal.point)
+
+    def extract_path(self, start_node,goal_node):
+        # Build the start-tree path, leaf to root (backwards) 
+        start_tree_path = []
+        while start_node is not None:
+            start_tree_path.append(start_node.point)
+            start_node = start_node.parent
+        
+        # Build the goal-tree path, leaf to root (forwards)
+        goal_tree_path = []
+        while goal_node is not None:
+            goal_tree_path.append(goal_node.point)
+            goal_node = goal_node.parent 
+
+        # First add the start path, reversing
+        overall_path = start_tree_path[::-1]
+        # Add the goal path
+        overall_path.extend(goal_tree_path)
+        return overall_path
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+                    prog='arm_rrt',
+                    description='Plans and executes paths for arms around obstacles.')
+    parser.add_argument('--filename',default='rrt_path.npy')           
+    parser.add_argument('-e', '--environment',default='middle')
+    parser.add_argument('-p', '--plan',action='store_true')
+    parser.add_argument('-r', '--run',action='store_true')
+    args = parser.parse_args()
+
+    rrt = RRT(env_name=args.environment)
+
+    if args.plan:
+        success = rrt.plan()
+
+        if success:
+            print("Tree planning reached the goal.")
+            np.save(args.filename,rrt.path_to_goal)
+        else:
+            print("Failed to find a path to the goal.")
+
+        rrt.controller.visTreesAndPaths( 
+            [rrt.start_tree,rrt.goal_tree],             
+            [rrt.path_to_goal],
+            rgbas_in=[[0.5,0.0,0.5,1.0],[0.902,0.106,0.714,1.0]]
+        )
+
+    if args.run:
+        path_to_goal = np.load(args.filename)
+        rrt.controller.execPath(path_to_goal)
+    
---
+++ robots.py
@@ -0,0 +1,217 @@
+import pybullet as pb
+import time
+import math
+from typing import List
+import numpy as np
+
+class Gen3LiteArmController(object):
+    """
+    A controller for the Kinova Gen3 Lite robotic arm in PyBullet.
+
+    This class provides methods to control the arm's joints and interact 
+    with it surrounding environment through collision checking.
+    """
+    def __init__(self, dt=1 / 50.0, env_name="Free"):
+        
+        self.dt = dt
+        pb.connect(pb.GUI,)
+        pb.setGravity(0, 0, -9.8)
+        pb.setTimeStep(self.dt)
+        pb.configureDebugVisualizer(pb.COV_ENABLE_GUI, 0)
+        pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 0)
+
+        # Load the Kinova Gen3 Lite URDF model.
+        # Ensure the path "gen3lite_urdf/gen3_lite.urdf" exists in your directory
+        self.__kinova_id = pb.loadURDF("gen3lite_urdf/gen3_lite.urdf", [0, 0, 0], useFixedBase=True)
+        pb.resetBasePositionAndOrientation(self.__kinova_id, [0, 0, 0.0], [0, 0, 0, 1])
+        self.END_EFFECTOR_INDEX = 7
+
+        self.__n_joints = 7 
+        self.__lower_limits: List = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
+        self.__upper_limits: List = [.967, 2, 2.96, 2.29, 2.96, 2.09, 3.05]
+        self.__home_poses: List = [math.pi, 0, 0.5 * math.pi, 0.5 * math.pi, 0.5 * math.pi, -math.pi * 0.5, 0]
+
+        self.joint_ids = [pb.getJointInfo(self.__kinova_id, i) for i in range(self.__n_joints)]
+        self.joint_ids = [j[0] for j in self.joint_ids if j[2] == pb.JOINT_REVOLUTE]
+
+        # This function creates obstacles and sets a reachable goal
+        self.createBalloonMaze(env_name)
+
+        # Set the viewer's camera viewpoint
+        pb.resetDebugVisualizerCamera(
+            cameraDistance=1.5,
+            cameraYaw=-120,
+            cameraPitch=-10,
+            cameraTargetPosition=self.goal_position
+        )
+
+    def getRanges(self):
+        return (self.__lower_limits,self.__upper_limits)
+    
+    def getCurrentJointAngles(self):
+        angles = []
+        for id in self.joint_ids:
+            joint_state = pb.getJointState(self.__kinova_id,id)
+            angles.append(joint_state[0])
+        return angles
+    
+    def setJointAngles(self, joint_angles):
+        for joint_index, q in enumerate(joint_angles):
+            pb.resetJointState(self.__kinova_id, joint_index, q)
+
+    def setToHome(self):
+        self.setJointAngles(self.__home_poses)
+
+    def fkine(self,angles):
+        # Record where we were (usually home, but just to be safe...)
+        curr = self.getCurrentJointAngles()
+        # Move the arm to the goal, specified in joint angles
+        self.setJointAngles(angles)
+        # Record the end effectors x,y,z position
+        state = pb.getLinkState(self.__kinova_id, self.END_EFFECTOR_INDEX)
+        # Move the arm back to where it began
+        self.setJointAngles(curr)
+
+        return state[0]
+
+    def execPath(self,path):
+        self.setJointAngles(self.__home_poses)
+        
+        pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1)
+        for p in path:
+            self.setJointAngles(p)
+            time.sleep(0.25)
+
+    def createBalloonMaze(self,env_name):
+        if env_name == "Hardest":
+            self.createHardestMaze()
+        elif env_name == "Easiest":
+            self.createEasiestMaze()
+        elif env_name == "Free":
+            self.createFreeMaze()
+
+    def createFreeMaze(self):
+        
+        # Initialize to home position.
+        self.setToHome()
+
+        # This is a hard-coded sensible goal to try to reach
+        self.goal_angles = [0.1026325022237283, -0.2931188624740633, 1.2717083400432991, 0.048794139164578594, 0.07744723004754135, -0.8437927483158898, -0.024709326684397483]
+
+        self.goal_position = self.fkine(self.goal_angles)
+
+        # Make a visual marker for the goal
+        goal_visual_id = pb.createVisualShape(pb.GEOM_BOX, 
+                                              halfExtents=[0.05,0.05,0.05],
+                                              rgbaColor=[0.0,0.0,1.0,0.5]
+                                              )
+        
+        pb.createMultiBody(baseMass=0, basePosition=self.goal_position, baseVisualShapeIndex=goal_visual_id )
+  
+    def createEasiestMaze(self):
+        self.createFreeMaze()
+        balloon_collision_id = pb.createCollisionShape(pb.GEOM_SPHERE, radius=0.07)
+        balloon_visual_id = pb.createVisualShape(pb.GEOM_SPHERE, radius=0.07,rgbaColor=[1.0,0.0,0.0,0.5])
+
+        x = 0.4
+        for y in [-0.125, 0.125]:
+            for z in [0.25, 0.5]:
+                box_id = pb.createMultiBody(baseMass=0, basePosition=[x, y, z],baseCollisionShapeIndex=balloon_collision_id,
+                baseVisualShapeIndex=balloon_visual_id  
+                )
+
+    def createHardestMaze(self):
+        self.createEasiestMaze()
+
+        balloon_collision_id = pb.createCollisionShape(pb.GEOM_SPHERE, radius=0.15)
+        balloon_visual_id = pb.createVisualShape(pb.GEOM_SPHERE, radius=0.15,rgbaColor=[1.0,0.0,0.0,0.5])
+
+        x = 0
+        for y in [-0.3, 0.3]:
+            for z in [0.2, 0.6]:
+                box_id = pb.createMultiBody(baseMass=0, basePosition=[x, y, z],baseCollisionShapeIndex=balloon_collision_id,
+                baseVisualShapeIndex=balloon_visual_id  
+                )        
+    
+    def visTreesAndPaths(self,trees,paths,rgbas_in):
+        
+        pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 0)
+
+        for tree_idx in range(len(trees)):
+            tree = trees[tree_idx]
+            rgba_in = rgbas_in[tree_idx]
+            for n in tree.node_list:
+                parent = n.parent
+                if not parent is None:
+                    self.setJointAngles(n.point)
+                    n_pos = pb.getLinkState(self.__kinova_id, self.END_EFFECTOR_INDEX)
+            
+                    self.setJointAngles(parent.point)
+                    parent_pos = pb.getLinkState(self.__kinova_id, self.END_EFFECTOR_INDEX)
+
+                    point_vis_id = pb.createVisualShape(pb.GEOM_SPHERE, radius=0.005,rgbaColor=rgba_in)
+                    node_sphere_id = pb.createMultiBody(baseMass=0, basePosition=n_pos[0], baseVisualShapeIndex=point_vis_id )
+
+                    pb.addUserDebugLine(lineFromXYZ=n_pos[0],lineToXYZ=parent_pos[0],lineColorRGB=rgba_in[0:3],lineWidth=0.01,lifeTime=0)
+
+        point_vis_id = pb.createVisualShape(pb.GEOM_SPHERE, radius=0.01,
+                                            rgbaColor=[1.0, 1.0, 0.0, 0.5])
+
+        for path in paths:
+            for idx in range(len(path)):
+                    
+                    parent_pos = self.fkine(path[idx])
+
+                    node_sphere_id = pb.createMultiBody(baseMass=0, basePosition=parent_pos, baseVisualShapeIndex=point_vis_id )
+
+                    if idx+1 < len(path):
+                        child_pos = self.fkine(path[idx+1])
+
+                        pb.addUserDebugLine(lineFromXYZ=parent_pos,lineToXYZ=child_pos,lineColorRGB=[1.0, 1.0, 0.0],lineWidth=0.02,lifeTime=0)
+        
+        self.setToHome()
+        pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1)
+        print("Finished plotting tree. Press q to continue.")
+
+        while True:
+            pb.stepSimulation()
+            time.sleep(1./240.)
+
+            keys = pb.getKeyboardEvents()
+    
+            if ord('q') in keys and keys[ord('q')] & pb.KEY_WAS_TRIGGERED:
+                print("Q pressed. Continuing.")
+                break
+
+    def collision_free(self,p1,p2):
+        
+        self.setJointAngles(p1)
+        if self.check_collision():
+            return False
+        self.setJointAngles(p2)
+        if self.check_collision():
+            return False
+        return True
+
+    def check_collision(self):
+        """
+        Checks for collisions between the robot and ANY other body in the environment, as well as self-collisions 
+        (robot links hitting each other).
+
+        Returns:
+            bool: True if any collision is detected, False otherwise.
+        """
+        # Ensure collision detection is up to date
+        pb.performCollisionDetection()
+
+        # Iterate over all bodies in the PyBullet simulation
+        for i in range(pb.getNumBodies()):
+            other_body_id = pb.getBodyUniqueId(i)
+            contact_points = pb.getContactPoints(bodyA=self.__kinova_id, bodyB=other_body_id)
+
+            # If contacts are found, return True immediately
+            if contact_points is None or len(contact_points) > 0:
+                return True
+
+        # If loop completes without returning, no collisions were found
+        return False
