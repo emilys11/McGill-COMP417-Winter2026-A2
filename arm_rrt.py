28553fc7818ce01ed3baaec8939f2471c0e2c3b1
emilys11 <emilysmelly11@gmail.com>
2026-02-08 01:28:35 -0500

    arm_rrt changes

--- arm_rrt.py
+++ arm_rrt.py
@@ -4,6 +4,7 @@ from scipy.spatial import cKDTree
 import random
 import argparse
 import robots
+import time #b)
 
 class Node:
     def __init__(self, point, parent=None):
@@ -14,16 +15,23 @@ class Tree:
     def __init__(self, node_list, kdtree):
         self.node_list = node_list
         self.kdtree = kdtree
-    
+        self.size = len(node_list) #b)
+
+
     def add(self,new_point):
+        
         self.node_list.append(new_point)
+
+        self.size += 1 #b)
         
         if len(self.node_list) % 50 == 0:
             data = [n.point for n in self.node_list]
             self.kdtree = cKDTree(data)
+    
+
 
 class RRT:
-    def __init__(self, step_size=0.1,max_iter=20,env_name="Free"):
+    def __init__(self, step_size=0.1,max_iter=6000,env_name="Hardest"):
         self.controller = robots.Gen3LiteArmController(env_name=env_name)
 
         self.start = Node(self.controller.getCurrentJointAngles())
@@ -35,38 +43,77 @@ class RRT:
         self.goal_tree = Tree([self.goal],cKDTree([self.goal.point]))
         self.path_to_goal = []
 
+        self.nodes_created = 0 #b)
+
+
     # This a "stub" planning function. It adds random nodes always
     # to the root of a start and goal tree. It shows many of the 
     # syntax elements and helper functions you could use, but 
     # you will have to fix and extend this to make it an RRT or 
     # RRT-Connect planner that can solve the harder environments.
     def plan(self):
-
-        # Add max_iter nodes to each start and goal tree
         for k in tqdm(range(self.max_iter)):
-            rnd_point = self.sample()
-            if self.collision_free(rnd_point, self.start.point):
-                new_node = Node(rnd_point,self.start)
-                self.start_tree.add(new_node)
+            #alternate between start and goal trees
+            if k % 2 == 0:
+                tree_a, tree_b = self.start_tree, self.goal_tree
+                swap = False
+            else:
+                tree_a, tree_b = self.goal_tree, self.start_tree
+                swap = True
+            
+            #sample random
+            x_rand = self.sample()
+            
+            #find nearest node in tree_a
+            x_near_a = self.nearest_node(x_rand, tree_a)
+            
+            #steer x_near_a 
+            x_new_a = self.steer(x_near_a, x_rand)
+            
+            #check if new_a is collision free
+            if self.collision_free(x_near_a.point, x_new_a.point):
 
-            rnd_point = self.sample()
-            if self.collision_free(rnd_point, self.goal.point):
-                new_node = Node(rnd_point,self.goal)
-                self.goal_tree.add(new_node)
+                #add new node to tree_a
+                x_new_a.parent = x_near_a
+                tree_a.add(x_new_a)
+                self.nodes_created += 1 #b)
+            
+                x_near_b = self.nearest_node(x_new_a.point, tree_b)
+                
+                while True:
+                    x_new_b = self.steer(x_near_b, x_new_a.point)
+                    
+                    #check if we can add this edge to tree_b
+                    if self.collision_free(x_near_b.point, x_new_b.point):
+                        x_new_b.parent = x_near_b
+                        tree_b.add(x_new_b)
+                        self.nodes_created += 1 #b)
 
-        # Now add the same rnd_pnt to both trees, making a connection
-        # and completing the path start->goal
-        rnd_point = self.sample()   
-        if self.collision_free(rnd_point, self.start.point):
-            new_start_node = Node(rnd_point,self.start)
-            self.start_tree.add(new_start_node)
+                        #check if trees are connected within reachable step size
+                        distance = np.linalg.norm(x_new_b.point - x_new_a.point)
+                        if distance < self.step_size:
+                            #found full path
+                            if swap:
+                                self.path_to_goal = self.extract_path(x_new_b, x_new_a)
+                            else:
+                                self.path_to_goal = self.extract_path(x_new_a, x_new_b)
 
-        if self.collision_free(rnd_point, self.goal.point):
-            new_goal_node = Node(rnd_point,self.goal)
-            self.goal_tree.add(new_goal_node)
+                            print(f"\n\nnumber of iterations: {k+1} iterations\n\n") #b)
+                            return True
+                        
+                        x_near_b = x_new_b
+                    else:
+                        #can't connect further because of a collision so we break
+                        break
+            
+            #update the trees if we swapped
+            if swap:
+                self.start_tree, self.goal_tree = tree_b, tree_a
+            else:
+                self.start_tree, self.goal_tree = tree_a, tree_b
 
-        self.path_to_goal = self.extract_path(new_start_node,new_goal_node)
-        return True
+        #solution not found
+        return False
 
     def sample(self):
         point = []
@@ -75,8 +122,11 @@ class RRT:
         return np.array(point)
 
     def nearest_node(self, point, tree):
-         _, idx = tree.kdtree.query(point)
-         return tree.node_list[idx]
+        t0 = time.time()
+        _, idx = tree.kdtree.query(point)
+        dt = time.time() - t0
+
+        return tree.node_list[idx]
 
     def steer(self, from_node, to_point):
         direction = to_point - from_node.point
@@ -122,6 +172,7 @@ class RRT:
 
 if __name__ == "__main__":
 
+
     parser = argparse.ArgumentParser(
                     prog='arm_rrt',
                     description='Plans and executes paths for arms around obstacles.')
@@ -134,7 +185,12 @@ if __name__ == "__main__":
     rrt = RRT(env_name=args.environment)
 
     if args.plan:
+        start_time = time.time() #b)
         success = rrt.plan()
+        elapsed = time.time() - start_time #b)
+
+        print(f"time elapsed: {elapsed:.3f} s") #b)
+        print(f"nodes created: {rrt.nodes_created}") #b)
 
         if success:
             print("Tree planning reached the goal.")
@@ -151,4 +207,5 @@ if __name__ == "__main__":
     if args.run:
         path_to_goal = np.load(args.filename)
         rrt.controller.execPath(path_to_goal)
+    
     
